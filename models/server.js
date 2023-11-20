2a1f62d2c348d4c0eef2e17c6620818459014d57
Santi-223 <128160456+Santi-223@users.noreply.github.com>
2023-11-19 21:15:45 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -24,8 +24,7 @@ class Server{
     middlewares() {
         this.app.use(cookieParser());
         this.app.use(express.static(__dirname + "/public"));
-        this.app.use( cors({
-  origin: 'http://localhost:60694'}) );
+        this.app.use( cors() );
         this.app.use(bodyParser.json())
     }
 
